b0626cfd48442ce1190049e771ab14cf9bc4dbf5
Novus <joemama@joemama.joe>
2022-10-10 20:57:07 +0000

    Add all files

---
+++ build.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+# Revision: 2022.10.01
+# (GNU/General Public License version 3.0)
+# by eznix (https://sourceforge.net/projects/ezarch/)
+
+# ----------------------------------------
+# Define Variables
+# ----------------------------------------
+
+LCLST="en_US"
+# Format is language_COUNTRY where language is lower case two letter code
+# and country is upper case two letter code, separated with an underscore
+
+KEYMP="us"
+# Use lower case two letter country code
+
+KEYMOD="pc105"
+# pc105 and pc104 are modern standards, all others need to be researched
+
+MYUSERNM="novos"
+# use all lowercase letters only
+
+MYUSRPASSWD="novos"
+# Pick a password of your choice
+
+RTPASSWD="novos"
+# Pick a root password
+
+MYHOSTNM="novos"
+# Pick a hostname for the machine
+
+# ----------------------------------------
+# Functions
+# ----------------------------------------
+
+# Test for root user
+rootuser () {
+  if [[ "$EUID" = 0 ]]; then
+    continue
+  else
+    echo "Please Run As Root"
+    sleep 2
+    exit
+  fi
+}
+
+# Display line error
+handlerror () {
+clear
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+}
+
+# Clean up working directories
+cleanup () {
+[[ -d ./ezreleng ]] && rm -r ./ezreleng
+[[ -d ./work ]] && rm -r ./work
+[[ -d ./out ]] && mv ./out ../
+sleep 2
+}
+
+# Requirements and preparation
+prepreqs () {
+pacman -S --noconfirm archlinux-keyring
+pacman -S --needed --noconfirm archiso mkinitcpio-archiso
+}
+
+# Copy ezreleng to working directory
+cpezreleng () {
+cp -r /usr/share/archiso/configs/releng/ ./ezreleng
+rm -r ./ezreleng/airootfs/etc/pacman.d/hooks
+rm ./ezreleng/airootfs/etc/motd
+rm -r ./ezreleng/grub
+rm -r ./ezreleng/efiboot
+rm -r ./ezreleng/syslinux
+}
+
+# Remove auto-login, cloud-init, hyper-v, ied, sshd, & vmware services
+rmunitsd () {
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/qemu-guest-agent.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/hv_fcopy_daemon.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/hv_kvp_daemon.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/hv_vss_daemon.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/vmware-vmblock-fuse.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/vmtoolsd.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/sshd.service
+rm ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/iwd.service
+rm -r ./ezreleng/airootfs/etc/systemd/system/cloud-init.target.wants
+}
+
+# Add cups, haveged, NetworkManager, & sddm systemd links
+addnmlinks () {
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/network-online.target.wants
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/printer.target.wants
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/sockets.target.wants
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/timers.target.wants
+mkdir -p ./ezreleng/airootfs/etc/systemd/system/sysinit.target.wants
+ln -sf /usr/lib/systemd/system/NetworkManager-wait-online.service ./ezreleng/airootfs/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
+ln -sf /usr/lib/systemd/system/NetworkManager-dispatcher.service ./ezreleng/airootfs/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
+ln -sf /usr/lib/systemd/system/NetworkManager.service ./ezreleng/airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service
+ln -sf /usr/lib/systemd/system/haveged.service ./ezreleng/airootfs/etc/systemd/system/sysinit.target.wants/haveged.service
+}
+
+# Copy files to customize the ISO
+cpmyfiles () {
+cp pacman.conf ./ezreleng/
+cp profiledef.sh ./ezreleng/
+cp packages.x86_64 ./ezreleng/
+cp -r grub ./ezreleng/
+cp -r efiboot ./ezreleng/
+cp -r syslinux ./ezreleng/
+cp -r etc ./ezreleng/airootfs/
+cp -r opt ./ezreleng/airootfs/
+cp -r usr ./ezreleng/airootfs/
+}
+
+# Set hostname
+sethostname () {
+echo "${MYHOSTNM}" > ./ezreleng/airootfs/etc/hostname
+}
+
+# Create passwd file
+crtpasswd () {
+echo "root:x:0:0:root:/root:/usr/bin/bash
+"${MYUSERNM}":x:1010:1010::/home/"${MYUSERNM}":/bin/bash" > ./ezreleng/airootfs/etc/passwd
+}
+
+# Create shadow file
+crtshadow () {
+root_hash=$(openssl passwd -6 "${RTPASSWD}")
+echo "root:"${root_hash}":14871::::::" > ./ezreleng/airootfs/etc/shadow
+}
+
+# create gshadow file
+crtgshadow () {
+echo "root:!*::root"
+}
+
+# Set the keyboard layout
+setkeylayout () {
+echo "KEYMAP="${KEYMP}"" > ./ezreleng/airootfs/etc/vconsole.conf
+}
+
+# Create 00-keyboard.conf file
+crtkeyboard () {
+mkdir -p ./ezreleng/airootfs/etc/X11/xorg.conf.d
+echo "Section \"InputClass\"
+        Identifier \"system-keyboard\"
+        MatchIsKeyboard \"on\"
+        Option \"XkbLayout\" \""${KEYMP}"\"
+        Option \"XkbModel\" \""${KEYMOD}"\"
+EndSection" > ./ezreleng/airootfs/etc/X11/xorg.conf.d/00-keyboard.conf
+}
+
+# Fix 40-locale-gen.hook and create locale.conf
+crtlocalec () {
+sed -i "s/en_US/"${LCLST}"/g" ./ezreleng/airootfs/etc/pacman.d/hooks/40-locale-gen.hook
+echo "LANG="${LCLST}".UTF-8" > ./ezreleng/airootfs/etc/locale.conf
+}
+
+# Start mkarchiso
+runmkarchiso () {
+mkarchiso -v -w ./work -o ./out ./ezreleng
+}
+
+# Clean up work and ezrelang
+cleanwork () {
+[[ -d ./ezreleng ]] && rm -r ./ezreleng
+[[ -d ./work ]] && rm -r ./work
+sleep 2
+}
+
+# ----------------------------------------
+# Run Functions
+# ----------------------------------------
+
+rootuser
+handlerror
+prepreqs
+cleanup
+cpezreleng
+addnmlinks
+rmunitsd
+cpmyfiles
+sethostname
+crtpasswd
+crtshadow
+crtgshadow
+setkeylayout
+crtkeyboard
+crtlocalec
+runmkarchiso
+cleanwork
+
+
+# Disclaimer:
+#
+# THIS SOFTWARE IS PROVIDED BY EZNIX “AS IS” AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL EZNIX BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# END
+#
